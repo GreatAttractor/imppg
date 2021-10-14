2e5f47c37c19a12f66ad2375c6a8eb9960bbed28
Marcel Hoffmann <github@pastsecurity.com>
2021-10-14 15:38:56 -0700

    German translation

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -316,6 +316,7 @@ void c_MainWindow::SelectLanguage()
         "polski",
         L"русский",
         L"українська",
+	"Deutsch",
 
         // After creating a new translation file, add the language name here
     };
@@ -325,9 +326,10 @@ void c_MainWindow::SelectLanguage()
         wxLANGUAGE_ENGLISH,
         wxLANGUAGE_POLISH,
         wxLANGUAGE_RUSSIAN,
-        wxLANGUAGE_UKRAINIAN
+        wxLANGUAGE_UKRAINIAN,
+	wxLANGUAGE_GERMAN
     };
-    const int NUM_LANGS_SUPPORTED = 4; // Has to be equal to number of elements in 'languageNames' and 'langIds'
+    const int NUM_LANGS_SUPPORTED = 5; // Has to be equal to number of elements in 'languageNames' and 'langIds'
     static_assert(NUM_LANGS_SUPPORTED == sizeof(languageNames) / sizeof(languageNames[0]));
     static_assert(NUM_LANGS_SUPPORTED == sizeof(langIds) / sizeof(langIds[0]));
 
@@ -385,14 +387,9 @@ void c_MainWindow::OnSaveFile()
     }
 
     wxFileDialog dlg(this, _("Save image"), Configuration::FileSavePath, wxEmptyString, GetOutputFilters(), wxFD_SAVE | wxFD_OVERWRITE_PROMPT);
-
-    const OutputFormat prev_output_format = Configuration::FileOutputFormat;
-    dlg.SetFilterIndex(static_cast<int>(prev_output_format));
-
     if (wxID_OK == dlg.ShowModal())
     {
         Configuration::FileSavePath = wxFileName(dlg.GetPath()).GetPath();
-        Configuration::FileOutputFormat = static_cast<OutputFormat>(dlg.GetFilterIndex());
         const c_Image processedImg = m_BackEnd->GetProcessedSelection();
         if (!processedImg.SaveToFile(dlg.GetPath().ToStdString(), static_cast<OutputFormat>(dlg.GetFilterIndex())))
         {
