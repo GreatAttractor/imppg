d5a67c4d20de76405bc1f288d40b6097d36caaf8
Filip Szczerek <ga.software@yahoo.com>
2023-04-26 21:32:46 +0200

    Work around FreeImage 3.19.0 + newer libtiff link error

--- src/image/src/image.cpp
+++ src/image/src/image.cpp
@@ -48,6 +48,48 @@ File description:
 #include <fitsio.h>
 #endif
 
+// Temporary workaround for FreeImage 3.19.0 and newer libtiff on Fedora 38.
+// FreeImage bug report: https://bugzilla.redhat.com/show_bug.cgi?id=2189506
+//
+// Putting it here as well (apart from image.cpp) to avoid `common_tests` linker error. I do not yet understand why
+// the one in `image.cpp` is not sufficient (it is for linking `imppg` itself, and there `-lfreeimage also occurs after
+// `libimage.a` in the linker invocation).
+//
+#if USE_FREEIMAGE && !defined(_TIFFDataSize)
+  #include <tiff.h>
+extern "C" {
+int
+_TIFFDataSize(TIFFDataType type)
+{
+    switch (type)
+    {
+    case TIFF_BYTE:
+    case TIFF_SBYTE:
+    case TIFF_ASCII:
+    case TIFF_UNDEFINED:
+        return 1;
+    case TIFF_SHORT:
+    case TIFF_SSHORT:
+        return 2;
+    case TIFF_LONG:
+    case TIFF_SLONG:
+    case TIFF_FLOAT:
+    case TIFF_IFD:
+    case TIFF_RATIONAL:
+    case TIFF_SRATIONAL:
+        return 4;
+    case TIFF_DOUBLE:
+    case TIFF_LONG8:
+    case TIFF_SLONG8:
+    case TIFF_IFD8:
+        return 8;
+    default:
+        return 0;
+    }
+}
+}
+#endif
+
 /// Conditionally swaps a 32-bit value
 uint32_t SWAP32cnd(uint32_t x, bool swap)
 {
